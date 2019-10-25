1786fa7d195a3917dfadc4c998b6723a179bd761
Davisdavy <davisdavy96@gmail.com>
2019-10-25 13:36:19 +0300

    complites recyclerview

--- app/src/main/java/com/davis/healthyliving/MainActivity.java
+++ app/src/main/java/com/davis/healthyliving/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> mCalories = new ArrayList<>();
+    private ArrayList<String> mFeatures= new ArrayList<>();
+    private ArrayList<String> mSource = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    private void initimageBitmaps(){
+        mImageUrls.add("https://images.mid-day.com/images/2019/mar/Bacon-Venue-a_l.jpg");
+        mImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS4SOmIk0pfHTDJIPfD_QxqJ7apsGLiOVjQgYP5aQSbjtn8Sq9x");
+        mImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQAJiCgenllJUFVwfyslBQUn2j7wqccWFb160sAbqm7UTdBXjx");
+        mImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSnvbQ-gdZukXtF-K5c4uDAOcASosKZuLD_6_uxbLgphsK2OiFv");
+        mImageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6YJ9nwlcL-KfiUcFLWmvvpMX4VVyNpe9VduhEyOndjLKMP2YT");
+        mImageUrls.add("https://images.mid-day.com/images/2019/mar/Bacon-Venue-a_l.jpg");
     }
 }
--- app/src/main/java/com/davis/healthyliving/RecyclerAdapter.java
+++ app/src/main/java/com/davis/healthyliving/RecyclerAdapter.java
@@ -1,4 +1,88 @@
 package com.davis.healthyliving;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerAdapter";
+    private ArrayList<String> mCalories = new ArrayList<>();
+    private ArrayList<String> mFeatures= new ArrayList<>();
+    private ArrayList<String> mSource = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerAdapter(Context mContext, ArrayList<String> mCalories, ArrayList<String> mFeatures, ArrayList<String> mSource, ArrayList<String> mImageUrls) {
+        this.mCalories = mCalories;
+        this.mFeatures = mFeatures;
+        this.mSource = mSource;
+        this.mImageUrls = mImageUrls;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items,parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.mImageview);
+
+        holder.mFeatureView.setText(mFeatures.get(position));
+        holder.mSourceView.setText(mSource.get(position));
+        holder.mCalories.setText(mCalories.get(position));
+
+        holder.mImageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on an image: " + mSource.get(position));
+                Toast.makeText(mContext, mSource.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSource.size();
+    }
+
+    public  class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView mImageview;
+        TextView mSourceView;
+        TextView mFeatureView;
+        TextView mCalories;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageview = itemView.findViewById(R.id.foodImage);
+            mFeatureView = itemView.findViewById(R.id.feature);
+            mCalories = itemView.findViewById(R.id.calorieCount);
+            mSourceView = itemView.findViewById(R.id.sourceName);
+        }
+    }
 }
